5606f362a9c4e4e71e42f281d665b450b80d7d69
dagimalemux <133212131+dagimalemux@users.noreply.github.com>
2023-12-18 13:59:56 +0000

    triangel shape

--- script.js
+++ script.js
@@ -1,23 +1,11 @@
 let canvas = document.querySelector("#canvas")
 let ctx = canvas.getContext("2d")
 
-ctx.strokeStyle = "dark-red"
-ctx.strokeRect(10,10,200,100)
+ctx.fillStyle="red"
 
-ctx.strokeStyle ="purple"
-ctx.strokeRect(20,20,180,80)
-
-ctx.strokeStyle ="blue"
-ctx.strokeRect(30,30,160,60)
-
-ctx.strokeStyle = "green"
-ctx.strokeRect(40,40,140,40)
-
-ctx.strokeStyle = "orange"
-ctx.strokeRect  (50,50,120,20)
-
-ctx.strokeStyle ="yellow"
-ctx.strokeRect  (60,60,100,10)
-
-ctx.strokeStyle = "black"
-ctx.strokeRect(59,59,90,3)
+ctx.beginPath();
+ctx.moveTo(100, 100)
+ctx.lineTo(150, 5)
+ctx.lineTo(200, 100)
+ctx.lineTo(100, 100)
+ctx.fill();
