d8bdad106802013dc50e97e51276f148976e2645
dagimalemux <133212131+dagimalemux@users.noreply.github.com>
2023-12-18 13:42:45 +0000

    canvas stroke style and rect

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-let canvas = document.querySelector("#canvas");
-let ctx = canvas.getContext("2d");
+let canvas = document.querySelector("#canvas")
+let ctx = canvas.getContext("2d")
 
-ctx.lineWidth = 2;
 ctx.strokeStyle = "red"
-ctx.strokeRect(10, 10, 200, 100)
+ctx.strokeRect(10,10,200,100)
