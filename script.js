007b5261986640c84a21864d2d48fd996a42e0ab
dagimalemux <133212131+dagimalemux@users.noreply.github.com>
2023-12-18 13:54:39 +0000

    adding more layers

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 let canvas = document.querySelector("#canvas")
 let ctx = canvas.getContext("2d")
 
-ctx.strokeStyle = "red"
+ctx.strokeStyle = "dark-red"
 ctx.strokeRect(10,10,200,100)
+
+ctx.strokeStyle ="purple"
+ctx.strokeRect(20,20,180,80)
+
+ctx.strokeStyle ="blue"
+ctx.strokeRect(30,30,160,60)
+
+ctx.strokeStyle = "green"
+ctx.strokeRect(40,40,140,40)
+
+ctx.strokeStyle = "orange"
+ctx.strokeRect  (50,50,120,20)
+
+ctx.strokeStyle ="yellow"
+ctx.strokeRect  (60,60,100,10)
+
+ctx.strokeStyle = "black"
+ctx.strokeRect(59,59,90,3)
